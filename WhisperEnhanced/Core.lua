8c17112e91a2913f45fd0be8bdfb6fba88f495a3
txpeaceofficer09 <james.mccaughey@gmail.com>
2025-12-04 04:29:42 -0600

    ...

--- WhisperEnhanced/Core.lua
+++ WhisperEnhanced/Core.lua
@@ -1,13 +1,19 @@
+local frame = CreateFrame("Frame")
+
 local whisperQueue = {}
 local whoPending = false
 local currentQuery = nil
 local PlayerData = {}
 local whoQueue = {}
 
-local function WhisperFilter(self, event, msg, sender, ...)
+function frame:print(msg)
+	print(("|cffffaaff[WHISPERENHANCED]:|r %s):format(msg))
+end
+
+local function ChatFilter(self, event, msg, sender, ...)
 	sender = sender:match("([^%-]+)")
 
-	--if event == "CHAT_MSG_WHISPER" then
+	if event == "CHAT_MSG_WHISPER" or event == "CHAT_MSG_WHISPER_INFORM" then
 		if PlayerData[sender] then
 			if PlayerData[sender].guild then
 				return false, ("[%s %d] <%s>: %s"):format(PlayerData[sender].class, PlayerData[sender].level, PlayerData[sender].guild, msg), sender, ...
@@ -20,88 +26,43 @@ local function WhisperFilter(self, event, msg, sender, ...)
 
 			return false, msg, sender, ...
 		end
-	--elseif event == "CHAT_MSG_WHISPER_INFORM" then
-		
-	--end
-end
-
-ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", WhisperFilter)
-ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", WhisperFilter)
-
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("WHO_LIST_UPDATE")
-
-local function ProcessWhoResult()
-    local found = false
-    local playerClass, playerLevel
-
-    for i = 1, GetNumWhoResults() do
-        local name, guild, level, race, class = GetWhoInfo(i)
-	PlayerData[name] = {
-		["guild"] = guild,
-		["level"] = level,
-		["race"] = race,
-		["class"] = class,
-	}
-	print("|cffffaa00[WHISPERENHANCED]:|r added |cff336699"..name.."|r player data.")
-	--[[
-        if name == currentQuery then
-            found = true
-            playerClass = class
-            playerLevel = level
-            break
-        end
-	]]
-    end
-
-	--[[
-    for i = #whisperQueue, 1, -1 do
-        local data = whisperQueue[i]
-        if data.sender == currentQuery then
-            local modifiedMsg
-
-            if found then
-                modifiedMsg = string.format("[%s %d] %s", playerClass, playerLevel, data.msg)
-            else
-                modifiedMsg = "[? ?] " .. data.msg
-            end
-
-            ChatFrame1:AddMessage(
-                string.format("|cffFFFF00(Delayed Whisper)|r [%s]: %s",
-                    data.sender, modifiedMsg
-                )
-            )
-
-            table.remove(whisperQueue, i)
-        end
-    end
-
-    currentQuery = nil
-    whoPending = false
-
-    if whisperQueue[1] then
-        currentQuery = whisperQueue[1].sender
-        whoPending = true
-        SendWho("n-" .. currentQuery)
-    end
-	]]
-end
-
-frame:SetScript("OnEvent", ProcessWhoResult)
-
-local function BlockWhoResults(self, event, msg, ...)
-	for k,v in ipairs(whoQueue) do
-		if msg:find(v) then
-			table.remove(whoQueue, k)
+	elseif event == "CHAT_MSG_SYSTEM" then
+		for k,v in ipairs(whoQueue) do
+			if msg:find(v) then
+				table.remove(whoQueue, k)
+				return true
+			end
+		end
+        
+		if msg:find("player total") then
 			return true
 		end
-	end
-        
-        if msg:find("player total") then
-            return true
-        end
 
-	return false, msg, ...
+		return false, msg, sender, ...
+	end
 end
 
-ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", BlockWhoResults)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", ChatFilter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", ChatFilter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", ChatFilter)
+
+local function OnEvent(self, event, ...)
+	if event == "WHO_LIST_UPDATE" then
+		self:print("Who list updated.")
+
+		local numResults = GetNumWhoResults()
+
+		for i = 1, GetNumWhoResults(), 1 do
+			local name, guild, level, race, class = GetWhoInfo(i)
+			PlayerData[name] = {
+				["guild"] = guild,
+				["level"] = level,
+				["race"] = race,
+				["class"] = class,
+			}
+		end
+	end
+end
+
+frame:RegisterEvent("WHO_LIST_UPDATE")
+frame:SetScript("OnEvent", OnEvent)
